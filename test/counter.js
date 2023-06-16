d9d03442efe3edf00ccd8017940b17ee7b3a5969
Emilio Sala <emilio_k88@hotmail.com>
2023-06-16 20:18:00 +0200

    formatting

--- test/counter.js
+++ test/counter.js
@@ -1,15 +1,15 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe('Counter Contract', () => {
-    it('Should increment the counter', async () => {
-      //ether to interact with smart contracts
-      const Counter = await ethers.getContractFactory('Counter');
-      //call our constructor
-      const counter = await Counter.deploy(0); 
+describe("Counter Contract", () => {
+  it("Should increment the counter", async () => {
+    //ether to interact with smart contracts
+    const Counter = await ethers.getContractFactory("Counter");
+    //call our constructor
+    const counter = await Counter.deploy(0);
 
-      await counter.increment(); 
-      const updateCounter = counter.getCounter();
-      expect(updateCounter).to.equal(1) //test
-   });
+    await counter.increment();
+    const updateCounter = counter.getCounter();
+    expect(updateCounter).to.equal(1); //test
+  });
 });
